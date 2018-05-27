d4d3708a386000cb3e1944b2eae49404b2a1d013
Irfan Ramadhi <ramadhiirfan@yahoo.co.id>
2018-05-27 19:56:36 +0700

    Unity Project Folder
    Add Gyroscope and Acclerometer function (+adding assets)

---
+++ Skeet Shooter 2D/Assets/Scripts/Gameplay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gameplay : MonoBehaviour
+{
+
+    public GyroControl gyroControl;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        transform.Translate(Input.acceleration.x * 0.05f, 0, 0);
+
+    }
+}
---
+++ Skeet Shooter 2D/Assets/Scripts/GyroControl.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GyroControl : MonoBehaviour
+{
+    public Text gyroText;
+
+    public float gyroXhandler;
+    public float gyroYhandler;
+    public float gyroZhandler;
+    private bool gyroEnabled;
+    private Gyroscope gyro;
+
+    private GameObject cameraContainer;
+    private Quaternion rot;
+
+
+    // Use this for initialization
+    private void Start()
+    {
+
+        cameraContainer = new GameObject("Camera Container");
+        cameraContainer.transform.position = transform.position;
+        transform.SetParent(cameraContainer.transform);
+        gyroEnabled = EnableGyro();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (gyroEnabled)
+        {
+            transform.localRotation = gyro.attitude * rot;
+            gyroXhandler = transform.localRotation.eulerAngles.x;
+            gyroYhandler = transform.localRotation.eulerAngles.y;
+            gyroZhandler = transform.localRotation.eulerAngles.z;
+            gyroText.text = (gyroXhandler.ToString()) + "\n" + (gyroYhandler.ToString()) + "\n" + (gyroZhandler.ToString());
+            Debug.Log(gyroText.text);
+        }
+
+    }
+
+    private bool EnableGyro()
+    {
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+
+            cameraContainer.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+            rot = new Quaternion(0, 0, 1, 0);
+
+            return true;
+        }
+        return false;
+    }
+
+    public void SetGyro()
+    {
+        //gyroText.text = transform.localRotation.ToString;
+    }
+
+}
