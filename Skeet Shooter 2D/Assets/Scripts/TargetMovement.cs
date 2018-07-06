d280124b7ba2527bcf624f89f9559066379b4d33
Irfan Ramadhi <ramadhiirfan@yahoo.co.id>
2018-07-06 09:46:59 +0700

    Checking code
    > Fixing gyroscope and accelerometer bug

---
+++ Skeet Shooter 2D/Assets/Scripts/TargetMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetMovement : MonoBehaviour {
+
+    public float speed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector2 dir = Vector2.zero;
+        dir.x = Input.acceleration.x;
+        dir.y = Input.acceleration.y;
+
+        if (dir.sqrMagnitude > 1)
+            dir.Normalize();
+        dir *= Time.deltaTime;
+        transform.Translate(dir * speed);
+
+	}
+}
